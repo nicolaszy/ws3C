cf32e8cad6f61be660ee356511580bf1c9801c6b
Christian Seiler <christian@christianseiler.ch>
2019-01-08 19:26:00 +0100

    add javascript scroll and sxml parsing for agenda

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 // https://www.w3schools.com/howto/howto_css_smooth_scroll.asp#section1
 
 $(document).ready(function () {
+    parseAgenda();
+
+
     // Add smooth scrolling to all links
     $("a").on('click', function (event) {
 
@@ -24,3 +27,45 @@ $(document).ready(function () {
         } // End if
     });
 });
+
+
+function parseAgenda() {
+    let request = new XMLHttpRequest();
+    request.onreadystatechange = function () {
+        if (this.readyState === 4 && this.status === 200) {
+            read(this);
+        }
+    };
+    request.open('GET', 'data/agenda.xml', true);
+    request.send();
+
+    function read(xml) {
+        let doc = xml.responseXML;
+        let agenda = document.getElementById('agenda');
+        let entries = doc.getElementsByTagName("entry");
+
+        for (let i = 0; i < entries.length; i++) {
+            let entry = entries[i];
+
+            let div = document.createElement('div');
+            div.className = "agendaentry";
+
+            let description = document.createElement('div');
+            description.className = "agendatitle";
+            description.innerHTML = entry.getElementsByTagName('description')[0].innerHTML;
+
+            let loc = entry.getElementsByTagName('location')[0];
+            let address = document.createElement('address');
+            let street = loc.getElementsByTagName('street')[0].innerHTML;
+            let plz = loc.getElementsByTagName('plz')[0].innerHTML;
+            let city = loc.getElementsByTagName('city')[0].innerHTML;
+            console.log(street);
+            address.innerHTML = street + '<br/>' + plz + " " + city;
+
+            div.append(description, address);
+            agenda.appendChild(div);
+        }
+
+
+    }
+}
