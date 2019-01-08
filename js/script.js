2762df6ea3917a3789d829d240943bdb649ca771
Christian Seiler <christian@christianseiler.ch>
2019-01-08 21:52:52 +0100

    Update agenda

--- js/script.js
+++ js/script.js
@@ -48,24 +48,63 @@ function parseAgenda() {
             let entry = entries[i];
 
             let div = document.createElement('div');
-            div.className = "agendaentry";
+            div.className = 'column';
 
-            let description = document.createElement('div');
-            description.className = "agendatitle";
+            let box = document.createElement('div');
+            box.className = 'box';
+
+            let title = document.createElement('h3');
+            title.className = "agendatitle has-background-info";
+            title.innerHTML = entry.getElementsByTagName('title')[0].innerHTML;
+
+            let date = document.createElement('p');
+            let d = entry.getElementsByTagName("date")[0];
+            if (d) {
+                let from = d.getAttribute('from');
+                let to = d.getAttribute('to');
+                let time = d.getAttribute('time');
+                date.innerHTML = createDateString(from, to, time);
+            }
+
+            let recuring = document.createElement('p');
+            let r = entry.getElementsByTagName('recuring')[0];
+            if (r) {
+                recuring.innerHTML = r.getAttribute('r');
+            }
+
+
+            let description = document.createElement('p');
             description.innerHTML = entry.getElementsByTagName('description')[0].innerHTML;
+
 
             let loc = entry.getElementsByTagName('location')[0];
             let address = document.createElement('address');
             let street = loc.getElementsByTagName('street')[0].innerHTML;
             let plz = loc.getElementsByTagName('plz')[0].innerHTML;
             let city = loc.getElementsByTagName('city')[0].innerHTML;
-            console.log(street);
             address.innerHTML = street + '<br/>' + plz + " " + city;
 
-            div.append(description, address);
+            box.append(title, date, recuring, description, address);
+            div.appendChild(box);
             agenda.appendChild(div);
         }
 
 
+    }
+
+    function createDateString(from, to, time) {
+        let result = "";
+        if (from) {
+            result += new Date(from).toLocaleDateString('de-CH');
+            if (to) {
+                result += " - " + new Date(to).toLocaleDateString('de-CH');
+            }
+        }
+
+        if (time) {
+            result += result.length > 0 ? " " : "";
+            result += time;
+        }
+        return result;
     }
 }
